c807068590b94c43d1991520cea8899f003986b9
Brendan Fogarty <fogarty27@gmail.com>
2015-10-13 20:53:35 -0700

    fixing git

---
+++ students/printing_ex/print_grid.py
@@ -0,0 +1,19 @@
+p = '+ '
+m = '- '
+b = '|         '
+top = p + (m * 4) + p + (m * 4) +p "+\n"
+bottom = b*3
+if 1 in 11
+    print(top)
+    print(bottom)
+    print(bottom)
+    print(bottom)
+    print(bottom)
+    print(top)
+    print(bottom)
+    print(bottom)
+    print(bottom)
+    print(bottom)
+    print(top)
+
+
