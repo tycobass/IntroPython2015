25e2a26ea7ea0b6b11c15f975bd295aebcf42026
Bob Jenkins <bob@hasoffers.com>
2015-10-31 13:30:32 -0700

    cleaned studlang

--- students/bob/session04/studlang.py
+++ students/bob/session04/studlang.py
@@ -25,31 +25,22 @@ def studlang():
 
     languages = {}
 
-    for line in source.read().split('\n'):
+    for line in source.read().split('\n'): ## source.readline() here was only grabbing a single character?
         try:
             name = line[:line.index(':')] # name = student (what comes before the colon)
         except ValueError:
-            print(None)
+            print('no ":" in your line: ', line)
         try:
             listy = line[line.index(':')+1:].split(' ') # create list of space-delimited languages (what comes after the colon)
         except ValueError:
-            print(None)
+            print('no ":" in your line: ', line)
         for lang in listy:
-            if lang not in languages:
-                languages[lang] = []
-            #print(lang)
-            languages[lang] += [name]
+            if lang not in languages: # if the language isn't already keyed
+                languages[lang] = []  # create it
+            languages[lang] += [name] # then add the student (name) to the item
     for lang in languages:
-        count = len(languages[lang])
-      #  print(count)
-      #  for numb in languages.values():
-      #      count = len(numb)
-        print(lang, ':', count)
+        count = len(languages[lang]) # count number of item's in a lang's value list
+        print(count, 'studied', lang)
 
 if __name__ == '__main__':
     print(studlang())
-
-
-##fix this:
-#for lang in languages:
-#    print (lang , sum(len(x) for x in lang.values().itervariables()))
