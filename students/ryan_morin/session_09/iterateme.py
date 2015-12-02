87f4052fc502aef5e5ca1882702d2b097501079a
Ryan Morin <ryanjmorin@gmail.com>
2015-12-01 18:34:18 -0800

    add iteratee.py lab

--- students/ryan_morin/session_09/iterateme.py
+++ students/ryan_morin/session_09/iterateme.py
@@ -5,14 +5,14 @@ class IterateMe_1(object):
     returns the sequence of numbers from zero to 4
     ( like range(4) )
     """
-    def __init__(self, stop=10, step=2):
+    def __init__(self, stop=10, step=1):
         self.current = -1
         self.stop = stop
         self.step = step
     def __iter__(self):
         return self
     def __next__(self):
-        self.current = (1 + self.current + self.step)
+        self.current = (self.current + self.step)
         if self.current < self.stop:
             return self.current
         else:
@@ -21,5 +21,10 @@ class IterateMe_1(object):
 if __name__ == "__main__":
 
     print("Testing the iterator")
-    for i in IterateMe_1():
+    it = IterateMe_1(20,2)
+    for i in it:
+        if i > 10: break
+        print (i)
+
+    for i in it:
         print(i)
