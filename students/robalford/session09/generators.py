220e8aedee57360cc2dfb084ed46980f5b30b541
robalford <robertsalford@gmail.com>
2015-12-06 10:36:28 -0800

    finished generators.py

--- students/robalford/session09/generators.py
+++ students/robalford/session09/generators.py
@@ -1,3 +1,6 @@
+from math import sqrt, ceil
+
+
 def sum_of_integers():
     total = 0
     x = 1
@@ -23,5 +26,25 @@ def fibonacci():
         b = a
         a = total
         total = a + b
+
+
+def prime_numbers():
+    # 2 is the first and only even prime number
+    yield 2
+    next_number = 3
+    prime = True
+    while prime:
+        # check if next number is divisible by every number between 1
+        # and it's own square route.
+        for n in range(2, ceil(sqrt(next_number))+1):
+            if next_number % n == 0:
+                prime = False
+        if prime:
+            yield next_number
+        # only check odd numbers
+        next_number += 2
+        # restart the loop by setting prime to True
+        prime = True
+
 
 
