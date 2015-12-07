31a5ea453720fa1f874f17943f082e2928d827a3
Eric Vegors <tycobass@gmail.com>
2015-12-07 17:09:08 +0000

    more key_test

--- students/eric_v/Session_08/Project/test_Directory_Crawler.py
+++ students/eric_v/Session_08/Project/test_Directory_Crawler.py
@@ -5,11 +5,11 @@ from Directory_Crawler import hashfile, file_hash_values
 
 
 def test_hashfile():
-    my_hash = hashfile("c:\work_area\Python\Info\loop.py", blocksize = 65536)
-    assert my_hash == '40608b9a2d1640de7366edc6db9ed69b'
+    my_hash = hashfile("key_test/key_test.txt", blocksize = 65536)
+    assert my_hash == 'd41d8cd98f00b204e9800998ecf8427e'
 
 def test_file_hash_values():
-    my_found_duplicates = file_hash_values('c:\work_area\Python\Info\Session_03')
+    my_found_duplicates = file_hash_values('key_test')
     print (my_found_duplicates)
-    'c:\\work_area\\Python\\Info\\Session_03\\list_lab.py' == my_found_duplicates['93c84386766ee905520b0005a7a3e080']
+    'key_test/frogs.txt' == my_found_duplicates['93c84386766ee905520b0005a7a3e080']
     #assert false
