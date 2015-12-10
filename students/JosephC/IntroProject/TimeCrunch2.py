771b7901ce0526943f6403682b00af5f53a545c3
JosephC <josephcardenas@gmail.com>
2015-12-09 19:14:03 -0800

    Updating TimeCrunch2 - now with basic functionality!

--- students/JosephC/IntroProject/TimeCrunch2.py
+++ students/JosephC/IntroProject/TimeCrunch2.py
@@ -16,33 +16,52 @@ then TimeCrunch will find time for you to develop good habits.
 #   create a calendar and a list of meetings and events
 calendar = []
 
-meeting1=list(rrule(DAILY, count=7,
-           dtstart=parse("20151214T090000"))) 
+
+meeting1 = list(rrule(DAILY, count=7,
+                dtstart=parse("20151214T090000"))) 
 
 
 meeting2 = list(rrule(DAILY, count=7,
-           dtstart = parse("20151214T103000")))
+                dtstart = parse("20151214T103000")))
 
 event1 = list(rrule(DAILY, count=3, 
-           dtstart = parse("20151211T130000")))
+              dtstart = parse("20151211T130000")))
+
+squats = list(rrule(SECONDLY, interval=30, count = 1)) 
+
+chair_squats = list(rrule(SECONDLY, interval = 30, count = 1))
+
+pushups = list(rrule(SECONDLY, interval = 45, count=1))
+
+crunches = list(rrule(SECONDLY, interval = 14, count = 1))
+
+arm_circles = list(rrule(SECONDLY, interval = 12, count = 1))
+
+leg_curl = list(rrule(SECONDLY, interval = 24, count = 1))
 
 
-calendar.extend(meeting1)
-calendar.extend(meeting2)
-calendar.extend(event1)
 
+your_events = [meeting1, meeting2, event1, squats, chair_squats, pushups, crunches, arm_circles, leg_curl]
 
-#so long as entries in workout are not in the list foo
-    #    check to see if there is room to place [not a time set]the items from 
-    #    into the list foo
+for d in your_events:
+    #print(d)
+    calendar.extend(d)
 
+sorted(calendar)
+"""
+time_format = "%a %b %d %H:%M:%S %Y"
+for i in calendar:
+    if i in calendar:
+        print("You have time to time to do {}!".format(today.strftime(time_format)))
+"""
+ 
+if squats in your_events: print("You have space to do a set of squats (it'll only take you 30 seconds!).")
+if chair_squats in your_events: print("You have space to a set of squats from your chair (it'll only take you 30 seconds!).")
+if pushups in your_events: print("You have space to do a set of pushups (it'll only take you 45 seconds!).")
+if crunches in your_events: print("You have space to do a set of crunches (it'll only take 14 seconds!).")
+if arm_circles in your_events: print("You have space to a round of arm circles (it'll only take 12 seconds1).")
+if leg_curl in your_events: print("You have space to do a set of leg curls (it'll only take 24 seconds per leg!).")
 
-
-#set two exercises, each with their own time interval in seconds 
-
-
-squats = list(rrule(SECONDLY, interval=30, count = 1))
-
-bench_press = list(rrule(SECONDLY, interval = 45, count=1))
+#Offer the use ability to look through their calendar, converting the wierd datetime format to something more human-readable
 
 
