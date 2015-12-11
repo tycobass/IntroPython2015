31b7fb92cdd6c313859e84e23beca5e464df4fe5
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-12-10 22:25:38 -0800

    cleaner

--- students/gabriel_meringolo/project/pack_list2.py
+++ students/gabriel_meringolo/project/pack_list2.py
@@ -6,12 +6,14 @@ from fractions import Fraction
 location = dirname(realpath(__file__))
 
 
-def main_menu():  # main menu
-    print(location)
+def main_menu():
+    """
+    main menu
+    """
     menu_choice = input("\nPack List V1.0\n---------------\n"
-                        "1. Use Existing List\n"  # works
-                        "2. Create New List\n"  # works just creates the .json file with name and empty json data
-                        "3. Exit\n\n>>> ")  # works
+                        "1. Use Existing List\n"
+                        "2. Create New List\n"
+                        "3. Exit\n\n>>> ")
     if menu_choice == "1":
         use_existing()
     elif menu_choice == "2":
@@ -23,14 +25,20 @@ def main_menu():  # main menu
         main_menu()
 
 
-def current_lists():  # displays all . json files in current dir
-        print("\nCurrent Lists:\n--------------")
-        for files in listdir(location):
-            if files.endswith(".json"):  # searches out .json files
-                print(files[:-5])  # removes the .json for ease of reading
+def current_lists():
+    """
+    displays all .json files in current dir
+    """
+    print("\nCurrent Lists:\n--------------")
+    for files in listdir(location):
+        if files.endswith(".json"):
+            print(files[:-5])
 
 
 def use_existing():
+    """
+    existing list selection/ checks if file exists
+    """
     current_lists()
     list_name = input("\nEnter list name: ")
     if list_name + ".json" in listdir(location):  #adds the .json to the name
@@ -47,13 +55,16 @@ def use_existing():
 
 
 def sub_menu(list_name):
+    """
+    sub menu
+    """
     print("\n" + list_name)
     sub_choice = input("-"*len(list_name)+"\n"
-                       "1. View List\n"  # works
-                       "2. Edit List\n"  # works?
+                       "1. View List\n"
+                       "2. Edit List\n"
                        "3. Generate Pack List\n"
-                       "4. Return To Main Menu\n"  # works
-                       "5. Exit Program\n\n>>> ")  # works
+                       "4. Return To Main Menu\n"
+                       "5. Exit Program\n\n>>> ")
     if sub_choice == "1":
         view_list(list_name)
     elif sub_choice == "2":
@@ -69,6 +80,9 @@ def sub_menu(list_name):
 
 
 def view_list(list_name):
+    """
+    displays content of json file
+    """
     data = read_json(list_name)
     print("\n Item                      Par\n-------------------------------")
     for i in data:
@@ -78,18 +92,27 @@ def view_list(list_name):
 
 
 def create_new_list(list_name):
+    """
+    creates new .json file containing '{}'
+    """
     with open(join(location, "{}.json".format(list_name)), "w") as f:
-        json.dump({}, f)  # creates emtpy json file with '{}' in it
+        json.dump({}, f)
     print("\nNew inventory file '{}' created.".format(list_name))
     edit_list(list_name)
 
 
-def exit_pl(): # exits program
+def exit_pl():
+    """
+    exits program
+    """
     print("Exiting Program\n")
     quit()
 
 
 def pack_list(list_name):
+    """
+    generates pack list
+    """
     data = read_json(list_name)
     par_check = OrderedDict()
     print("\n")
@@ -108,6 +131,9 @@ def pack_list(list_name):
 
 
 def edit_list(list_name):
+    """
+    edits inventory list
+    """
     while True:
         item = input("Enter new item: ")
         par = input("Set par: ")
@@ -119,6 +145,9 @@ def edit_list(list_name):
 
 
 def update_json_file(list_name, item, par):
+    """
+    updates json file
+    """
     data = read_json(list_name)
     data[item] = par
     with open(join(location, "{}.json".format(list_name)), "w") as f:
@@ -126,6 +155,9 @@ def update_json_file(list_name, item, par):
 
 
 def read_json(list_name):
+    """
+    reads json and returns data
+    """
     with open(join(location, "{}.json".format(list_name)), "r") as f:
         return json.load(f, object_pairs_hook=OrderedDict)
 
