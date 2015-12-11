b19db32644c34d0a2989165c35ea38585e01ee20
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2015-12-10 19:46:19 -0800

    works, needs alot of refactoring and cleaning up and unit tests. would also like to add more features

--- students/gabriel_meringolo/project/pack_list2.py
+++ students/gabriel_meringolo/project/pack_list2.py
@@ -1,5 +1,5 @@
 import json
-from measurement.measures import volume
+# from measurement.measures import volume 
 from os.path import dirname, realpath, join
 from os import listdir
 from collections import OrderedDict
@@ -7,13 +7,13 @@ from fractions import Fraction
 location = dirname(realpath(__file__))
 
 
-def main_menu(): # main menu
+def main_menu():  # main menu
     print(location)
-    menu_choice = input("\nPack List V0.03\n"
+    menu_choice = input("\nPack List V1.0\n"
                         "---------------\n"
-                        "1. Use Existing List\n" # works
-                        "2. Create New List\n" # works just creates the .json file with name and empty json data
-                        "3. Exit\n\n>>> ") # works
+                        "1. Use Existing List\n"  # works
+                        "2. Create New List\n"  # works just creates the .json file with name and empty json data
+                        "3. Exit\n\n>>> ")  # works
     if menu_choice == "1":
         use_existing()
     elif menu_choice == "2":
@@ -25,18 +25,18 @@ def main_menu(): # main menu
         main_menu()
 
 
-def current_lists(): # displays all . josn files in current dir
+def current_lists():  # displays all . json files in current dir
         print("\nCurrent Lists:\n"
               "--------------")
         for files in listdir(location):
-            if files.endswith(".json"): # searches out .json files
-                print(files[:-5]) # removes the .json for ease of reading
+            if files.endswith(".json"):  # searches out .json files
+                print(files[:-5])  # removes the .json for ease of reading
 
 
 def use_existing():
     current_lists()
     list_name = input("\nEnter list name: ")
-    if list_name + ".json" in listdir(location): #adds the .json to the name
+    if list_name + ".json" in listdir(location):  #adds the .json to the name
         sub_menu(list_name)
     else:
         make_choice = input("\n\aList does not exist, create new list? Y/N ")
@@ -52,11 +52,11 @@ def use_existing():
 def sub_menu(list_name):
     print("\n" + list_name)
     sub_choice = input("-"*len(list_name)+"\n"
-                       "1. View List\n" # works
-                       "2. Edit List\n" # works?
+                       "1. View List\n"  # works
+                       "2. Edit List\n"  # works?
                        "3. Generate Pack List\n"
-                       "4. Return To Main Menu\n" # works
-                       "5. Exit Program\n\n>>> ") # works
+                       "4. Return To Main Menu\n"  # works
+                       "5. Exit Program\n\n>>> ")  # works
     if sub_choice == "1":
         view_list(list_name)
     elif sub_choice == "2":
@@ -78,6 +78,7 @@ def view_list(list_name):
           "-------------------------------")
     for i in data:
         print("{:<25} {}".format(i + ":", data[i]))
+    input("\nHit 'Enter' to continue.\n")
     sub_menu(list_name)
 
 
@@ -94,13 +95,22 @@ def exit_pl(): # exits program
 
 
 def pack_list(list_name):
-    current_amts = OrderedDict()
     with open(join(location, "{}.json".format(list_name)), "r") as f:
         data = json.load(f, object_pairs_hook=OrderedDict)
-    print("Please enter current amounts of the following..\n")
+    par_check = OrderedDict()
+    print("\n")
     for i in data:
-        current_amt = input("{} ".format(i))
-        current_amts[i] = current_amt
+        current_amt = input("Current amount of {}: ".format(i))
+        if "/" in current_amt:
+            current_amt = str(round(float(Fraction(current_amt)), 2))
+        par_check[i] = current_amt
+    print("\n Pack list for: {}\n".format(list_name) +
+          "-----------------" + "-" * len(list_name))
+    for i in par_check:
+        if float(par_check[i]) >= float(data[i]):
+            print("{}: good".format(i))
+        elif float(par_check[i]) < float(data[i]):
+            print(i + ":", round(float(float(data[i]) - float(par_check[i])), 2))
 
 
 def edit_list(list_name):
@@ -123,5 +133,6 @@ def update_json_file(list_name, item, par):
         json.dump(data, f, indent=4)
 
 
-main_menu()
+if __name__ == "__main__":
+    main_menu()
 
